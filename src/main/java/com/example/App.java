f62eb1813decf997788ab95b66ba279dac96c0f3
priyadharshini526 <priyakarupusamy490@gmail.com>
2026-04-13 14:30:18 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,13 @@
 package com.example;
 
-/**
- * Hello world!
- */
 public class App {
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+
+    // 👇 NEW METHOD (IMPORTANT)
+    public static int add(int a, int b){
+        return a + b;
     }
 }
